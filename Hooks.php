06b7e35f1ad95d3f3bb150dce791f928f0900ee8
scribu <mail@scribu.net>
2011-07-31 01:47:50 +0300

    add  variable to scbHooks::debug()

--- Hooks.php
+++ Hooks.php
@@ -1,6 +1,7 @@
 <?php
 
 class scbHooks {
+	private static $mangle_name;
 
 	public static function add( $class ) {
 		self::_do( 'add_filter', $class );
@@ -10,17 +11,23 @@ class scbHooks {
 		self::_do( 'remove_filter', $class );
 	}
 
-	public static function debug( $class ) {
+	public static function debug( $class, $mangle_name = false ) {
+		self::$mangle_name = $mangle_name;
+
 		echo "<pre>";
 		self::_do( array( __CLASS__, '_print' ), $class );
 		echo "</pre>";
 	}
 
 	private static function _print( $tag, $callback, $prio, $argc ) {
-		if ( is_object( $callback[0] ) )
-			$class = '$' . get_class( $callback[0] );
-		else
+		$static = !is_object( $callback[0] );
+
+		if ( self::$mangle_name )
+			$class = $static ? '__CLASS__' : '$this';
+		else if ( $static )
 			$class = "'" . $callback[0] . "'";
+		else
+			$class = '$' . get_class( $callback[0] );
 
 		$func = " array( $class, '$callback[1]' )";
 
