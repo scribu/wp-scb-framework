ad377d3e3f50fd055f23d0cb20813ba0f224ad31
scribu <mail@scribu.net>
2011-11-23 01:01:41 +0200

    introduce scb_register_table(), scb_install_table() and scb_uninstall_table()

--- Table.php
+++ Table.php
@@ -8,14 +8,11 @@ class scbTable {
 	protected $upgrade_method;
 
 	function __construct( $name, $file, $columns, $upgrade_method = 'dbDelta' ) {
-		global $wpdb;
-
 		$this->name = $name;
 		$this->columns = $columns;
 		$this->upgrade_method = $upgrade_method;
 
-		$wpdb->tables[] = $name;
-		$wpdb->$name = $wpdb->prefix . $name;
+		scb_register_table( $name );
 
 		if ( $file ) {
 			scbUtil::add_activation_hook( $file, array( $this, 'install' ) );
@@ -24,36 +21,50 @@ class scbTable {
 	}
 
 	function install() {
-		global $wpdb;
-
-		$full_table_name = $wpdb->prefix . $this->name;
-
-		$charset_collate = '';
-		if ( $wpdb->has_cap( 'collation' ) ) {
-			if ( ! empty( $wpdb->charset ) )
-				$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
-			if ( ! empty( $wpdb->collate ) )
-				$charset_collate .= " COLLATE $wpdb->collate";
-		}
-
-		if ( 'dbDelta' == $this->upgrade_method ) {
-			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-			dbDelta( "CREATE TABLE $full_table_name ( $this->columns ) $charset_collate" );
-			return;
-		}
-
-		if ( 'delete_first' == $this->upgrade_method )
-			$wpdb->query( "DROP TABLE IF EXISTS $full_table_name;" );
-
-		$wpdb->query( "CREATE TABLE IF NOT EXISTS $full_table_name ( $this->columns ) $charset_collate;" );
+		scb_install_table( $this->name, $this->columns, $this->upgrade_method );
 	}
 
 	function uninstall() {
-		global $wpdb;
-
-		$full_table_name = $wpdb->prefix . $this->name;
-
-		$wpdb->query( "DROP TABLE IF EXISTS $full_table_name" );
+		scb_uninstall_table( $this->name );
 	}
+}
+
+
+function scb_register_table( $name ) {
+	global $wpdb;
+
+	$wpdb->tables[] = $name;
+	$wpdb->$name = $wpdb->prefix . $name;
+}
+
+function scb_install_table( $name, $columns, $upgrade_method = 'dbDelta' ) {
+	global $wpdb;
+
+	$full_table_name = $wpdb->$name;
+
+	$charset_collate = '';
+	if ( $wpdb->has_cap( 'collation' ) ) {
+		if ( ! empty( $wpdb->charset ) )
+			$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+		if ( ! empty( $wpdb->collate ) )
+			$charset_collate .= " COLLATE $wpdb->collate";
+	}
+
+	if ( 'dbDelta' == $upgrade_method ) {
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( "CREATE TABLE $full_table_name ( $columns ) $charset_collate" );
+		return;
+	}
+
+	if ( 'delete_first' == $upgrade_method )
+		$wpdb->query( "DROP TABLE IF EXISTS $full_table_name;" );
+
+	$wpdb->query( "CREATE TABLE IF NOT EXISTS $full_table_name ( $columns ) $charset_collate;" );
+}
+
+function scb_uninstall_table( $name ) {
+	global $wpdb;
+
+	$wpdb->query( "DROP TABLE IF EXISTS $wpdb->$name" );
 }
 
