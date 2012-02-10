c02b280e707201ad25448fa1d92be652c9224348
scribu <mail@scribu.net>
2012-02-10 23:53:59 +0200

    describe do_activation() and do_uninstall()

--- Util.php
+++ Util.php
@@ -40,6 +40,7 @@ class scbUtil {
 			add_action( 'scb_activation_' . plugin_basename( $plugin ), $callback );
 	}
 
+	// For debugging
 	static function do_activation( $plugin ) {
 		do_action( 'scb_activation_' . plugin_basename( $plugin ) );
 	}
@@ -52,6 +53,7 @@ class scbUtil {
 		add_action( 'uninstall_' . plugin_basename( $plugin ), $callback );
 	}
 
+	// For debugging
 	static function do_uninstall( $plugin ) {
 		do_action( 'uninstall_' . plugin_basename( $plugin ) );
 	}
