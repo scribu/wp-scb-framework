3224a04f64e8f61231fd6ea8c30c9ded3e92b8c4
scribu <mail@scribu.net>
2013-05-10 13:39:10 +0300

    bump revision to 59

--- load.php
+++ load.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['_scb_data'] = array( 58, __FILE__, array(
+$GLOBALS['_scb_data'] = array( 59, __FILE__, array(
 	'scbUtil', 'scbOptions', 'scbForms', 'scbTable',
 	'scbWidget', 'scbAdminPage', 'scbBoxesPage', 'scbPostMetabox',
 	'scbCron', 'scbHooks',
