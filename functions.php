c2f09b2c27750b1132b9948bddf2c3ac9711da03
Andrey Savchenko <contact@rarst.net>
2013-05-28 00:02:34 +0300

    Added composer.json and pass through loader function.

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Pass through version to use when Composer handles classes load.
+ *
+ * @param callable $callback
+ */
+function scb_init( $callback = null ) {
+	if ( $callback )
+		call_user_func( $callback );
+}
