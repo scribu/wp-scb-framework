4d5cf3e836fb67eaba22a673c7ded1adf68b9b3e
meloniq <meloniq@gmail.com>
2014-12-22 21:19:58 +0100

    doc the scbTable class

--- Table.php
+++ Table.php
@@ -1,12 +1,37 @@
 <?php
-
-// Takes care of creating, updating and deleting database tables
-
+/**
+ * Takes care of creating, updating and deleting database tables.
+ */
 class scbTable {
+
+	/**
+	 * The table name.
+	 * @var string
+	 */
 	protected $name;
+
+	/**
+	 * The table columns.
+	 * @var string
+	 */
 	protected $columns;
+
+	/**
+	 * The upgrade method.
+	 * @var string
+	 */
 	protected $upgrade_method;
 
+	/**
+	 * Setups table.
+	 *
+	 * @param string $name Table name.
+	 * @param string $file Reference to main plugin file.
+	 * @param string $columns The SQL columns for the CREATE TABLE statement.
+	 * @param array $upgrade_method (optional)
+	 *
+	 * @return void
+	 */
 	public function __construct( $name, $file, $columns, $upgrade_method = 'dbDelta' ) {
 		$this->name = $name;
 		$this->columns = $columns;
@@ -20,10 +45,20 @@ class scbTable {
 		}
 	}
 
+	/**
+	 * Installs table.
+	 *
+	 * @return void
+	 */
 	public function install() {
 		scb_install_table( $this->name, $this->columns, $this->upgrade_method );
 	}
 
+	/**
+	 * Uninstalls table.
+	 *
+	 * @return void
+	 */
 	public function uninstall() {
 		scb_uninstall_table( $this->name );
 	}
